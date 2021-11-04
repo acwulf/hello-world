0a73bee86401f95305ec87f2a6190dc47affb4bf
acwulf <acwulf@pm.me>
2021-11-04 19:00:15 -0400

    add up to problem0003

--- project-euler/problem0001.py
+++ project-euler/problem0001.py
@@ -4,18 +4,14 @@ The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
-max_num = 10
+max_num = 1000 
 mult1 = 3
 mult2 = 5
 sum_naturals = 0
 
-#rules = [i % mult1 == 0,
-#        i % mult2 == 0 ]
-
-for i in range(max_num):
-    #if all(rules):
+for i in range(1, max_num):
     if (i % mult1 == 0) or (i % mult2 == 0):
-        print(i)
-        #sum_naturals += i
+        #print(i)
+        sum_naturals += i
 
 print(sum_naturals)
